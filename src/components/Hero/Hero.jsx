7f5ecab5d19ccfbb86168dfb2b8958c490157175
Violeta <vnaboishchykova@gmail.com>
2024-03-25 05:31:01 +0100

    hero_story-section_adaptation

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -3,16 +3,15 @@ import img from '../../images/hero/hero.png'
 export const Hero = () => {
     return (
     <section className={styles.hero}>
-            <div>
+            <div className={styles.titleWrapper}>
                 <h2 className={styles.subtitle}>diD yOu seE iT ?</h2>
                 <h1 className={styles.title}>YACHT APES</h1>
                 <h2 className={styles.subtitle}>Apes aRe eveRywhere</h2>
             </div> 
             <img src={img} alt="A monkey" className={styles.photoHero} />
-            <div>
-                <h3 className={styles.name}>MEET APES</h3>
+            <div className={styles.btnWrapper}>
+                <h3 className={styles.btn}>MEET APES</h3>
                 <p>Yacht Ape is a collection of unique digital apes that you can own in NFT format</p>
-
             </div>
     </section>
 )
